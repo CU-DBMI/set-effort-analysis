7a33372cf20ef124cf33f49af75778b8562129fc
d33bs <dave.bunten@cuanschutz.edu>
2023-12-01 13:28:56 -0700

    add set-specific images and analysis

--- set_effort_analysis/dbmi-related-software-landscape.py
+++ set_effort_analysis/dbmi-related-software-landscape.py
@@ -68,6 +68,15 @@ org_names = Box.from_yaml(
 # show how many orgs we need to gather data from
 len(org_names)
 
+# %%
+# gather set team member data
+dbmi_set_github_usernames = Box.from_yaml(
+    filename=f"{data_dir}/software-landscape/github-dbmi-set-members.yaml"
+).github_users
+
+# show set team username len
+len(dbmi_set_github_usernames)
+
 
 # %%
 def get_github_org_or_user(
@@ -190,6 +199,9 @@ df_github_metrics.info()
 # %%
 # Function to find the top language for each row
 def find_top_language(languages):
+    """
+    Finds top language given a dictionary of GitHub-detected languages.
+    """
     if isinstance(languages, dict):
         non_empty_languages = {
             key: value for key, value in languages.items() if value is not None
@@ -397,22 +409,8 @@ fig_dependencies.show()
 # %%
 programming_language_counts.head(15)
 
-
 # %%
 # add the top language per repo
-def find_top_language(languages):
-    """
-    Finds top language given a dictionary of GitHub-detected languages.
-    """
-    if isinstance(languages, dict):
-        non_empty_languages = {
-            key: value for key, value in languages.items() if value is not None
-        }
-        if non_empty_languages:
-            return max(non_empty_languages, key=non_empty_languages.get)
-    return None
-
-
 github_metrics = [
     dict(
         repo,
@@ -468,3 +466,223 @@ for programming_lang in programming_language_counts.head(15)["Primary language"]
     fig_dependencies.show()
 
 # %%
+# form listing of only set members
+github_metrics_set_contrib_only = [
+    repo
+    for repo in github_metrics
+    if any(
+        [
+            set_member
+            in [member["login"] for member in repo["GitHub Contributor Members"]]
+            for set_member in dbmi_set_github_usernames.to_list()
+        ]
+    )
+]
+ak.Array(github_metrics_set_contrib_only)
+
+# %%
+df_github_metrics_set_contrib_only = pd.DataFrame(github_metrics_set_contrib_only)
+# gather the number of lines of code
+df_github_metrics_set_contrib_only["Total lines of GitHub detected code"] = (
+    df_github_metrics_set_contrib_only["GitHub Detected Languages"]
+    .dropna()
+    .apply(lambda x: sum(value if value is not None else 0 for value in x.values()))
+)
+
+# Apply the function to the "GitHub Detected Languages" column and create a new column "Primary programming language"
+df_github_metrics_set_contrib_only[
+    "Primary language"
+] = df_github_metrics_set_contrib_only["GitHub Detected Languages"].apply(
+    find_top_language
+)
+df_github_metrics_set_contrib_only.info()
+
+# %%
+# prep for creating an hbar chart for primary languages
+grouped_data = (
+    df_github_metrics_set_contrib_only.groupby(["Primary language"])
+    .size()
+    .reset_index(name="Count")
+)
+
+# Group by "Primary programming language" and calculate the sum of counts for each programming language
+programming_language_counts = (
+    grouped_data.groupby("Primary language")["Count"].sum().reset_index()
+)
+
+# Sort programming languages by the sum of counts in descending order
+programming_language_counts = programming_language_counts.sort_values(
+    by="Count", ascending=False
+)
+programming_language_counts
+
+# Create a horizontal bar chart for primary language counts
+fig_languages = px.bar(
+    data_frame=programming_language_counts.sort_values(by="Count"),
+    title=f"Repository Primary Language Count",
+    y="Primary language",
+    x="Count",
+    color_discrete_sequence=[color_seq[0]],
+    text="Count",
+    orientation="h",
+    width=1200,
+    height=700,
+)
+
+# Customize layout to display count labels properly
+fig_languages.update_traces(
+    texttemplate="%{text}",
+    textposition="inside",
+)
+fig_languages.update_layout(
+    # ensure all y axis labels appear
+    yaxis=dict(
+        tickmode="array",
+        tickvals=programming_language_counts["Primary language"].tolist(),
+        ticktext=programming_language_counts["Primary language"].tolist(),
+    ),
+)
+
+fig_languages.write_image(
+    "images/software-landscape-primary-language-counts-set-only.png"
+)
+fig_languages.show()
+
+# %%
+# Create a horizontal bar chart for language line count totals
+# gather total lines of code for all repos by language
+total_language_line_counts = [
+    {
+        "language": language,
+        "line_count": ak.sum(
+            ak.Array(github_metrics_set_contrib_only)["GitHub Detected Languages"][
+                language
+            ]
+        ),
+    }
+    for language in ak.Array(github_metrics_set_contrib_only)[
+        "GitHub Detected Languages"
+    ].fields
+]
+
+df_total_language_line_counts = pd.DataFrame.from_records(
+    total_language_line_counts
+).sort_values(by="line_count")
+
+df_total_language_line_counts
+df_total_language_line_counts = df_total_language_line_counts.sort_values(
+    by="line_count"
+)
+
+fig_languages = px.bar(
+    data_frame=df_total_language_line_counts,
+    title=f"Repository Language Line Counts Total",
+    y="language",
+    x="line_count",
+    color_discrete_sequence=[color_seq[2]],
+    text="line_count",
+    orientation="h",
+    width=1200,
+    height=1200,
+)
+
+# Customize layout to display count labels properly
+fig_languages.update_traces(
+    texttemplate="%{text}",
+    textposition="inside",
+)
+fig_languages.update_layout(
+    xaxis_title="Lines of Code (Total)",
+    yaxis_title="Language",
+    # ensure all y axis labels appear
+    yaxis=dict(
+        tickmode="array",
+        tickvals=df_total_language_line_counts["language"].tolist(),
+        ticktext=df_total_language_line_counts["language"].tolist(),
+    ),
+)
+
+fig_languages.write_image(
+    "images/software-landscape-language-line-counts-total-set-only.png"
+)
+fig_languages.show()
+
+# %%
+df_total_language_line_counts["line_count_log"] = np.log(
+    df_total_language_line_counts["line_count"]
+)
+
+# Create a horizontal bar chart for language line count totals
+fig_languages = px.bar(
+    data_frame=df_total_language_line_counts.sort_values(by="line_count_log"),
+    title=f"Repository Language Line Counts Total",
+    y="language",
+    x="line_count_log",
+    color_discrete_sequence=[color_seq[2]],
+    text="line_count_log",
+    orientation="h",
+    width=1200,
+    height=1200,
+)
+
+# Customize layout to display count labels properly
+fig_languages.update_traces(
+    texttemplate="%{text}",
+    textposition="inside",
+)
+fig_languages.update_layout(
+    xaxis_title="Lines of Code (Total)(log)",
+    yaxis_title="Language",
+    # ensure all y axis labels appear
+    yaxis=dict(
+        tickmode="array",
+        tickvals=df_total_language_line_counts["language"].tolist(),
+        ticktext=df_total_language_line_counts["language"].tolist(),
+    ),
+)
+
+fig_languages.write_image(
+    "images/software-landscape-language-line-counts-total-log-set-only.png"
+)
+fig_languages.show()
+
+# %%
+aarr = ak.Array(github_metrics_set_contrib_only)
+for programming_lang in programming_language_counts.head(15)["Primary language"]:
+    masked = aarr.mask[aarr["Primary Programming Language"] == programming_lang]
+
+    # value count the SBOM dependencies in total
+    flat_array = ak.flatten(masked["GitHub Repo SBOM"]["sbom"]["packages"]["name"])
+    flat_list = ak.to_list(flat_array)
+    counts = Counter(flat_list)
+    df_dependency_counts = pd.DataFrame(
+        list(counts.items()), columns=["Dependency", "Occurrence Count"]
+    )
+    df_dependency_counts_top = df_dependency_counts.sort_values(
+        by="Occurrence Count", ascending=False
+    ).head(100)
+
+    fig_dependencies = px.bar(
+        data_frame=df_dependency_counts_top.sort_values(by="Occurrence Count"),
+        title=f"Repository Dependency Counts Total for {programming_lang}",
+        y="Dependency",
+        x="Occurrence Count",
+        color_discrete_sequence=[color_seq[4]],
+        text="Occurrence Count",
+        orientation="h",
+        width=1200,
+        height=1300,
+    )
+    fig_dependencies.update_layout(
+        # ensure all y axis labels appear
+        yaxis=dict(
+            tickmode="array",
+            tickvals=df_dependency_counts_top["Dependency"].tolist(),
+            ticktext=df_dependency_counts_top["Dependency"].tolist(),
+        ),
+    )
+
+    fig_dependencies.write_image(
+        f"images/software-landscape-dependency-counts-total-{programming_lang}-set-only.png"
+    )
+    fig_dependencies.show()
